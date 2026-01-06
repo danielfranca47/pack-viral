87e71404785051e400da2d84bde9019ba3673d81
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-06 20:13:31 +0200

    feat: Implementando a estrutura básica e objetivos do projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
-
 export default function Home() {
-   return <div className="min-h-dvh bg-zinc-50  dark:bg-black"></div>;
+   return (
+      <main>
+         {/* TODO: Implementar a seção inicial */}
+         <section></section>
+         {/* TODO: Implementar a seção dos nichos em alta */}
+         <section></section>
+         {/* TODO: Implementar a seção do que mudou no pack viral */}
+         <section></section>
+         {/* TODO: Implementar a seção do que mais o cliente irá receber */}
+         <section></section>
+         {/* TODO: Implementar a seção dos testemunhos dos clientes */}
+         <section></section>
+         {/* TODO: Implementar a seção do bônus que será recebido */}
+         <section></section>
+         {/* TODO: Implementar a seção dos planos do curso */}
+         <section></section>
+         {/* TODO: Implementar a seção da garantia do curso */}
+         <section></section>
+         {/* TODO: Implementar a seção das dúvidas frequentes */}
+         <section></section>
+         {/* TODO: Implementar a seção do CTA para o whatsapp */}
+         <section></section>
+      </main>
+   );
 }
