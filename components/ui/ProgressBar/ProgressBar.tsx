fd4fa552d659552ed3c581d9e9ea5d45a8ff71a1
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-12 13:42:21 +0200

    feat: Adicionando o componente da barra de progresso para mostrar a percentagem das pessoas que já aproveitaram a oferta

---
+++ components/ui/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useEffect, useRef } from "react";
+import styles from "./ProgressBar.module.css";
+
+const ProgressBar = () => {
+   const parentContainer = useRef<HTMLDivElement>(null);
+
+   useEffect(() => {
+      const element = parentContainer.current as Element;
+      function handleIntersection(entries: IntersectionObserverEntry[]) {
+         const [entry] = entries;
+         if (entry.isIntersecting) {
+            element?.classList.toggle(styles.progressoVisto);
+         }
+      }
+      const observer = new IntersectionObserver(handleIntersection, { root: null, rootMargin: "0px", threshold: 0.5 });
+      if (parentContainer) observer.observe(element);
+
+      // Clean Up
+      return () => {
+         if (parentContainer) observer.unobserve(element);
+      };
+   }, [parentContainer]);
+
+   return (
+      <div className="bg-zinc-100 w-full my-7 rounded" ref={parentContainer}>
+         <div className={`py-2 bg-tema text-end pe-4 rounded-s font-medium  ${styles.progresso}`}>97%</div>
+      </div>
+   );
+};
+export default ProgressBar;
