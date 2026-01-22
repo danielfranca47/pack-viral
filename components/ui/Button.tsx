a0d1b18e50b851cf957b3e7a4fb63b35ffe72537
Renzi Fidele <renzifidele2001@gmail.com>
2026-01-22 20:09:06 +0200

    feat: Mudando a  cor global para verde

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 const Button = ({ children, className }: { children: ReactNode; className?: string }) => {
    return (
       <button
-         className={`bg-[#FF751E] text-[21px] sm:text-[27px] text-white px-6 py-3 rounded font-semibold sombraBtn cursor-pointer hover:-translate-y-1.25 transition ${className}`}
+         className={`bg-[#00B419] text-[21px] sm:text-[27px] text-white px-6 py-3 rounded font-semibold sombraBtn cursor-pointer hover:-translate-y-1.25 transition ${className}`}
       >
          {children}
       </button>
