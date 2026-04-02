c8726ad8fb795c30c58ba0c80ca04f9765ac570c
Renzi Fidele <renzifidele2001@gmail.com>
2026-04-02 17:56:59 +0200

    feat: Persistindo o tempo de progresso do vídeo e informando o usuário se deseja continuar assistindo ou se pretende reiniciar

--- components/ui/VideoPlayer.tsx
+++ components/ui/VideoPlayer.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { VolumeOff } from "lucide-react";
+import { Play, Repeat, VolumeOff } from "lucide-react";
 import Player from "next-video";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const videoUrl = "https://res.cloudinary.com/dtra2u08q/video/upload/v1775124216/Cakto_packviral.online_nbijpd.mp4";
 
 const VideoPlayer = () => {
    const [clicaste, setClicaste] = useState(false);
+   const [assistiu, setAssistiu] = useState(false);
+   const [analiseConcluida, setAnaliseConcluida] = useState(false);
+
    const videoRef = useRef<HTMLVideoElement | null>(null);
 
    function handleButtonClick() {
@@ -14,30 +17,90 @@ const VideoPlayer = () => {
          setClicaste(true);
          videoRef.current.muted = false;
          videoRef.current.volume = 1;
-         videoRef.current.currentTime = -1;
+         videoRef.current.currentTime = 0;
       }
    }
 
+   function continuarAssistindo() {
+      if (videoRef?.current) {
+         videoRef.current.muted = false;
+         videoRef.current.volume = 1;
+         videoRef.current.currentTime = Number(localStorage.getItem("tempoAssistido"));
+         setAssistiu(false);
+         setClicaste(true);
+      }
+   }
+
+   function assistirDoInicio() {
+      if (videoRef?.current) {
+         videoRef.current.muted = false;
+         videoRef.current.volume = 1;
+         videoRef.current.currentTime = 0;
+         setAssistiu(false);
+         setClicaste(true);
+      }
+   }
+
+   useEffect(() => {
+      // Verifica se o usuário já assistiu ou não
+      const tempoAssistido = localStorage.getItem("tempoAssistido");
+      function verificarSeAssistiu() {
+         if (Number(tempoAssistido) > 10) setAssistiu(true);
+         setAnaliseConcluida(true);
+      }
+      verificarSeAssistiu();
+
+      // Atualiza o progresso no localStora a cada 10 segundos
+      const atualizar = setInterval(() => {
+         console.log(String(videoRef?.current?.currentTime));
+         localStorage.setItem("tempoAssistido", String(videoRef?.current?.currentTime));
+      }, 10000);
+
+      // Clean-up do timer
+      return () => {
+         clearInterval(atualizar);
+      };
+   }, []);
+
+   /* Renderiza o player após verificar se o usuário assistiu */
    return (
-      <div className="w-fit relative flex items-center justify-center">
-         <Player
-            autoPlay
-            muted
-            ref={videoRef}
-            src={videoUrl}
-            className="**:mx-auto aspect-auto! *:border *:border-tema *:w-[85%]! md:*:w-120! xl:*:w-130!"
-         />
-         {!clicaste && (
-            <div
-               onClick={handleButtonClick}
-               className="absolute text-center flex flex-col items-center bg-violet-500 p-5 gap-2 cursor-pointer border rounded opacity-90"
-            >
-               <p className="font-semibold">Seu vídeo já começou</p>
-               <VolumeOff className="size-12" />
-               <p>Clique para ouvir</p>
-            </div>
-         )}
-      </div>
+      analiseConcluida && (
+         <div className="w-fit relative flex items-center justify-center">
+            <Player
+               autoPlay
+               muted
+               ref={videoRef}
+               src={videoUrl}
+               className="**:mx-auto aspect-auto! *:border *:border-tema *:w-[85%]! md:*:w-120! xl:*:w-130!"
+            />
+
+            {assistiu ? (
+               <div className="absolute inset-0 bg-violet-500 flex flex-col items-center justify-center gap-4 [&>div]:flex [&>div]:items-center [&>div]:gap-2 [&>div]:cursor-pointer [&>div]:hover:scale-110 [&>div]:transition">
+                  <p className="font-semibold mb-2">Você já começou a assistir esse vídeo</p>
+                  <div onClick={continuarAssistindo}>
+                     <Play />
+                     <span>Continuar assistindo?</span>
+                  </div>
+                  <hr className="border-white w-50 border-dashed" />
+                  <div onClick={assistirDoInicio}>
+                     <Repeat />
+                     <span>Assistir do início?</span>
+                  </div>
+               </div>
+            ) : (
+               !clicaste && (
+                  <div
+                     onClick={handleButtonClick}
+                     className="absolute text-center flex flex-col items-center bg-violet-500 p-5 gap-2 cursor-pointer border rounded opacity-90"
+                  >
+                     <p className="font-semibold">Seu vídeo já começou</p>
+                     <VolumeOff className="size-12" />
+                     <p>Clique para ouvir</p>
+                  </div>
+               )
+            )}
+         </div>
+      )
    );
 };
 export default VideoPlayer;
